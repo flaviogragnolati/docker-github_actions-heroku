cc5d12f8e6470042bb1d0eb9fd1ea6c62677a9aa
flavio <flavio.gragnolati@gmail.com>
2022-04-16 21:08:14 -0300

    config file created

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import express from 'express';
-import dotenv from 'dotenv';
+import config from './config';
 
-dotenv.config();
+const { secret, port } = config;
 
 const app = express();
-
-const port = process.env.PORT || 6060;
-const secret = process.env.SECRET || 'ENV NOT LOADED';
 
 app.get('/', (req, res) => {
   return res.status(200).send(`Hello World!!!. This is a Secret ${secret}`);
