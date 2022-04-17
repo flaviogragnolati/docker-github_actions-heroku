b6698f8d2c0046fee7e01ea085ffff3dfaf50506
flavio <flavio.gragnolati@gmail.com>
2022-04-17 13:16:44 -0300

    added health route & dockerize job

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   return res.status(200).send(`Hello World!!!. This is a Secret ${secret}`);
 });
 
+app.get('/health', (req, res) => {
+  return res.status(200).send(`OK`);
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
